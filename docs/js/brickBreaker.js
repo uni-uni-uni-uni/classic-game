356bac695e654d9075f198fafe577e51a4d402e1
uni <never.odd.or.even.00.00@gmail.com>
2024-08-22 22:42:25 +0900

    0822 ボールがブロックで反射、ブロック消滅

--- docs/js/brickBreaker.js
+++ docs/js/brickBreaker.js
@@ -39,10 +39,10 @@ function generateBlock(fieldWidth, rowCount, colCount, r, c){
     const block = document.createElement('div');
     block.classList.add('block');
 
-    const blockWidth = (fieldWidth - config.block.margin * (colCount + 1) - 2 * colCount) / colCount ;
-    block.style.width = `${blockWidth}px`;
+    // const blockWidth = (fieldWidth - config.block.margin * (colCount + 1) - 2 * colCount) / colCount ;
+    block.style.width = `${config.block.style.width}px`;
     block.style.height = `${config.block.style.height}px`;
-    block.style.left = `${c * (blockWidth + config.block.margin + 2) + config.block.margin}px`; //borderSize*2=2
+    block.style.left = `${c * (config.block.style.width + config.block.margin + 2) + config.block.margin}px`; //borderSize*2=2
     block.style.top = `${r * (20 + config.block.margin) + config.block.margin}px`;
 
     return block;
@@ -69,8 +69,8 @@ function generateBall(){
     ball.setAttribute('id','ball');
     ball.style.width = `${config.ball.radius * 2}px`;
     ball.style.height = `${config.ball.radius * 2}px`;
-    ball.style.left = `${0}px`;
-    ball.style.top = `${50}px`;
+    ball.style.left = `${150}px`;
+    ball.style.top = `${300}px`;
 
     field.append(ball);
     return ball;
@@ -82,8 +82,14 @@ function doTick(){
         const dPos = nomalizeVector(config.ball.angle, config.ball.speed)
         moveBall(dPos.x, dPos.y);
 
+        // ブロック衝突判定
+        const targetIndex = checkCollision();
+        if(targetIndex != null){
+            obj.blocks[targetIndex].remove();
+            obj.blocks.splice(targetIndex, 1);
+        }
 
-        // 反転チェック
+        // 壁衝突判定
         checkInvert();
 
     }, 1000 * 1 / 60);
@@ -111,6 +117,99 @@ function moveBall(dx, dy){
     obj.ball.style.top = `${pos.y + dy}px`;
 }
 
+function checkCollision(){
+    const pos = getPosOf(obj.ball);
+    const dPos = nomalizeVector(config.ball.angle, config.ball.speed);
+    pos.x = pos.x + dPos.x + config.ball.radius;
+    pos.y = pos.y + dPos.y + config.ball.radius;
+
+    for (let i = 0; i < obj.blocks.length; i++) {
+        const block = obj.blocks[i];
+        const blockStartPos = getPosOf(block);
+        const blockEndPos = {
+            x:blockStartPos.x + config.block.style.width,
+            y:blockStartPos.y + config.block.style.height,
+        };
+
+        let socre = 0;
+        if(pos.x < blockStartPos.x){
+            socre = socre + 1;
+        }
+        if(pos.y < blockStartPos.y){
+            socre = socre + 2;
+        }
+        if(pos.x > blockEndPos.x){
+            socre = socre + 4;
+        }
+        if(pos.y > blockEndPos.y){
+            socre = socre + 8;
+        }
+
+        if(socre == 1){
+            if(distance(pos.x, 0, blockStartPos.x, 0) < config.ball.radius){
+                config.ball.angle = -config.ball.angle;
+                return i;
+            }
+        }else if(socre == 2){
+            if(distance(0, pos.y, 0, blockStartPos.y) < config.ball.radius){
+                config.ball.angle = (180 - config.ball.angle) % 360;
+                return i;
+            }
+        }else if(socre == 3){
+            if(distance(pos.x, pos.y, blockStartPos.x, blockStartPos.y) < config.ball.radius){
+                if((blockStartPos.x - pos.x) - (blockStartPos.y - pos.y) < 0){
+                    config.ball.angle = (180 - config.ball.angle) % 360;
+                }else{
+                    config.ball.angle = -config.ball.angle;
+                }
+                return i;
+            }
+        }else if(socre == 4){
+            if(distance(pos.x, 0, blockEndPos.x, 0) < config.ball.radius){
+                config.ball.angle = -config.ball.angle;
+                return i;
+            }
+        }else if(socre == 6){
+            if(distance(pos.x, pos.y, blockEndPos.x, blockStartPos.y) < config.ball.radius){
+                if((pos.x - blockEndPos.x) - (blockStartPos.y - pos.y) < 0){
+                    config.ball.angle = (180 - config.ball.angle) % 360;
+                }else{
+                    config.ball.angle = -config.ball.angle;
+                }
+                return i;
+            }
+        }else if(socre == 8){
+            if(distance(0, pos.y, 0, blockEndPos.y) < config.ball.radius){
+                config.ball.angle = (180 - config.ball.angle) % 360;
+                return i;
+            }
+        }else if(socre == 9){
+            if(distance(pos.x, pos.y, blockStartPos.x, blockEndPos.y) < config.ball.radius){
+                if((blockStartPos.x - pos.x) - (pos.y - blockEndPos.y) < 0){
+                    config.ball.angle = (180 - config.ball.angle) % 360;
+                }else{
+                    config.ball.angle = -config.ball.angle;
+                }
+                return i;
+            }
+        }else if(socre == 12){
+            if(distance(pos.x, pos.y, blockEndPos.x, blockEndPos.y) < config.ball.radius){
+                if((pos.x - blockEndPos.x) - (pos.y - blockEndPos.y) < 0){
+                    config.ball.angle = (180 - config.ball.angle) % 360;
+                }else{
+                    config.ball.angle = -config.ball.angle;
+                }
+                return i;
+            }
+        }
+    }
+    return null;
+}
+
+function distance(a,b,c,d){
+    return ((c-a)**2 + (d-b)**2)**(1/2)
+}
+
 function nomalizeVector(angle, speed){
     const radian = angle * (Math.PI / 180);
     return {
@@ -129,9 +228,6 @@ function checkInvert(){
         config.ball.angle = (180 - config.ball.angle) % 360;
     }
 }
-
-
-
 
 init();
 
