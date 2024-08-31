78ea15affdf651f9bc8c5abce659985eb6688f6b
uni <never.odd.or.even.00.00@gmail.com>
2024-08-31 21:37:37 +0900

    index清書した　commonに追記

--- docs/js/common.js
+++ docs/js/common.js
@@ -32,3 +32,15 @@ function getPosOf(obj){
         top:Number(getTopOf(obj)),
     };
 }
+
+function movable(obj) {
+    obj.onpointermove = function(event){
+        if(event.buttons){
+            this.style.left     = this.offsetLeft + event.movementX + 'px';
+            this.style.top      = this.offsetTop  + event.movementY + 'px';
+            this.style.position = 'absolute';
+            this.draggable      = false;
+            this.setPointerCapture(event.pointerId);
+        }
+    }
+}
